c0c3b404abfe5b5129eadd8ce2d313deb02e8597
Shivam <kumarshivam3788@gmail.com>
2024-12-30 20:22:02 +0530

    Implement GET API for fetching user message acceptance status

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -61,3 +61,54 @@ export async function POST(request:Request){
         ) 
     }
 }
+
+export async function GET(){
+    dbConnect()
+
+    const session = await getServerSession(authOptions)
+
+    const user : User = session?.user
+
+    if (!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message:'Not authenticated',
+            },
+            { status: 401 }
+        ) 
+    }
+
+    const userId = user._id
+
+    try {
+        const findUser = await UserModel.findById(userId)
+    
+        if(!findUser){
+            return Response.json(
+                {
+                    success: false,
+                    message:'failed to found user',
+                },
+                { status: 404 }
+            )
+        }
+    
+        return Response.json(
+            {
+                success: true,
+                isAcceptingMessages:findUser.isAcceptingMessage
+            },
+            { status: 200 }
+        ) 
+    } catch (error) {
+        console.log("failed to update user status to accept messages",error)
+        return Response.json(
+            {
+                success: false,
+                message:'Error in getting message acceptance status',
+            },
+            { status: 500 }
+        ) 
+    }
+}
